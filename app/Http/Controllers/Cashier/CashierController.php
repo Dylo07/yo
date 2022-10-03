c9251e0c445ac08f4e6d49f490a98789db9f53a6
Your Name <you@example.com>
2022-10-03 23:21:30 +0530

    last here comes

--- app/Http/Controllers/Cashier/CashierController.php
+++ app/Http/Controllers/Cashier/CashierController.php
@@ -50,7 +50,7 @@ class CashierController extends Controller
         $html = '';
         foreach($menus as $menu){
             $html .= '
-            <div class="col-md-auto ">
+            <div class="mr-1 mt-1">
                 <a class="btn btn-outline-secondary btn-menu" data-id="'.$menu->id.'">
                     <img class="img-fluid" src="'.url('/menu_images/'.$menu->image).'">
                     <br>

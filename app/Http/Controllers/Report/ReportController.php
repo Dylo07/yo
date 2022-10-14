1e51acf6424229869d13c6bdf07477bca7ef4365
Taleem Hussain <taleem35@gmail.com>
2022-10-14 16:51:09 +0500

    added category in the summary
    added category in the summary

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Report;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Sale;
+use App\Models\Category;
+
 use App\Exports\SaleReportExport;
 use Maatwebsite\Excel\Facades\Excel;
 use DB;
@@ -24,13 +26,19 @@ class ReportController extends Controller
         $dateEnd = date("Y-m-d H:i:s", strtotime($request->dateEnd.' 23:59:59'));
 
         $sales = Sale::whereBetween('updated_at', [$dateStart, $dateEnd])->where('sale_status','paid');
-        
-        $summarySales = Sale::select('menu_id','menu_name', DB::raw('SUM(sale_details.quantity) as qty_sum'))
+      
+        $summarySales = Sale::select('menu_id','menu_name','categories.name', DB::raw('SUM(sale_details.quantity) as qty_sum'))
         ->join('sale_details', 'sales.id', '=', 'sale_details.sale_id')
+        ->join('menus', 'menus.id', '=', 'sale_details.menu_id')
+        ->join('categories', 'categories.id', '=', 'menus.category_id')
+        
         ->whereBetween('sales.updated_at', [$dateStart, $dateEnd])
         ->where('sales.sale_status','paid')
         ->where('sale_details.quantity','>','0')
-        ->groupBy('sale_details.menu_id','menu_name')->get();
+
+        ->groupBy('sale_details.menu_id','menu_name','categories.name')
+        ->orderby('categories.name','asc')
+        ->get();
 
 
         return view('report.showReport')->with('dateStart', date("m/d/Y H:i:s", strtotime($request->dateStart.' 00:00:00')))

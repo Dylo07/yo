22b8d32aa6bea3266c5acc6efe7c0ee9cc345544
Your Name <you@example.com>
2022-10-18 09:32:38 +0530

    last auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
 @section('content')
 <div class="container">
     <div class="row justify-content-center">

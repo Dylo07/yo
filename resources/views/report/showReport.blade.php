540b3e2416af7d23c842e7cac20b3cae1cffbd89
Dylo07 <dilankaseniya@gmail.com>
2026-02-14 19:13:29 +0530

    Fix: remove duplicate/slim jQuery that was overwriting full jQuery and breaking $.ajax

--- resources/views/report/showReport.blade.php
+++ resources/views/report/showReport.blade.php
@@ -194,13 +194,10 @@
   </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.0.1/js/tempusdominus-bootstrap-4.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/js/tempusdominus-bootstrap-4.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {

6ea47f26eaa20432369e9c25196fbe7f303f4f23
Dylo07 <dilankaseniya@gmail.com>
2026-03-01 18:44:15 +0530

    Fix mobile cards not showing on mobile devices
    - Removed inline style='display: none;' from mobile-vehicle-cards div
    - CSS media queries now properly control visibility
    - Desktop (>992px): Table shown, cards hidden via CSS !important
    - Mobile (<992px): Cards shown, table hidden via media query
    - Fixes issue where inline style was preventing mobile view from working

--- resources/views/vehicle-security/index.blade.php
+++ resources/views/vehicle-security/index.blade.php
@@ -342,8 +342,8 @@
    </div>
 </div>
 
-<!-- Mobile Card View (Hidden on Desktop) -->
-<div class="mobile-vehicle-cards" id="mobileVehicleCards" style="display: none;">
+<!-- Mobile Card View -->
+<div class="mobile-vehicle-cards" id="mobileVehicleCards">
     @foreach($vehicles as $vehicle)
     <div class="vehicle-card {{ $vehicle->checkout_time ? 'status-checked-out' : ($vehicle->is_temp_out ? 'status-temp-out' : 'status-on-property') }} {{ $vehicle->team ? 'team-'.str_replace(' ', '', $vehicle->team) : '' }}" 
          data-vehicle-id="{{ $vehicle->id }}">
